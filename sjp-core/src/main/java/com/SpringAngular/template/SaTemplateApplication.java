d482a1de2eab2fb3dd991247ac90f2bf8bf5ace7
Hleyf <hleyf@hotmail.com>
2024-02-27 14:29:59 +0000

    project setup and main JAR architecture.

---
+++ sjp-core/src/main/java/com/SpringAngular/template/SaTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.SpringAngular.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaTemplateApplication.class, args);
+	}
+
+}
---
+++ sjp-core/src/test/java/com/SpringAngular/template/SaTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringAngular.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SaTemplateApplicationTests.class)
+class SaTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
